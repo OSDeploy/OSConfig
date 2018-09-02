62ab9f4f40a3c1637aa264c46782b711de48d7e7
David Segura <david@segura.org>
2018-09-02 01:18:18 -0500

    18.9.2

--- LocalPolicy/Import-GPO.ps1
+++ LocalPolicy/Import-GPO.ps1
@@ -162,8 +162,8 @@ if (Test-Path $LGPO) {
 	#	Gather LGPO Policies
 	#======================================================================================
 	$GPOPolicies = Get-ChildItem "$ScriptDirectory\ImportGPO" -Directory
-	if ($ProductName -like "*Windows 7*") {$GPOPolicies = $GPOPolicies | Where-Object {$_.FullName -NotLike "*Win10*"}}
-	if ($ProductName -like "*Windows 10*") {$GPOPolicies = $GPOPolicies | Where-Object {$_.FullName -NotLike "*Win7*"}}
+	if ($ProductName -like "*Windows 7*") {$GPOPolicies = $GPOPolicies | Where-Object {$_.FullName -NotLike "*Win10*" -and $_.FullName -NotLike "*Server*"}}
+	if ($ProductName -like "*Windows 10*") {$GPOPolicies = $GPOPolicies | Where-Object {$_.FullName -NotLike "*Win7*" -and $_.FullName -NotLike "*Server*"}}
 	if ($ProductName -like "*Windows Server") {$GPOPolicies = $GPOPolicies | Where-Object {$_.FullName -NotLike "*Win7*" -and $_.FullName -NotLike "*Win10*"}}
 	#======================================================================================
 	#	Import LGPO Policies
@@ -176,8 +176,8 @@ if (Test-Path $LGPO) {
 	# Gather Registry XML
 	#======================================================================================
 	$GPOPreferences = Get-ChildItem "$ScriptDirectory\ImportGPO" Registry.xml -Recurse
-	if ($ProductName -like "*Windows 7*") {$GPOPreferences = $GPOPreferences | Where-Object {$_.FullName -NotLike "*Win10*"}}
-	if ($ProductName -like "*Windows 10*") {$GPOPreferences = $GPOPreferences | Where-Object {$_.FullName -NotLike "*Win7*"}}
+	if ($ProductName -like "*Windows 7*") {$GPOPreferences = $GPOPreferences | Where-Object {$_.FullName -NotLike "*Win10*" -and $_.FullName -NotLike "*Server*"}}
+	if ($ProductName -like "*Windows 10*") {$GPOPreferences = $GPOPreferences | Where-Object {$_.FullName -NotLike "*Win7*" -and $_.FullName -NotLike "*Server*"}}
 	if ($ProductName -like "*Windows Server") {$GPOPolicies = $GPOPolicies | Where-Object {$_.FullName -NotLike "*Win7*" -and $_.FullName -NotLike "*Win10*"}}
 	#======================================================================================
 	# Import Registry XML
